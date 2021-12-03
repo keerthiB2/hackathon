3fdad3d209fde8509a60db69dd94d0b778cad543
Bairi Keerthi <keerthibairi8@gmail.com>
2021-12-03 16:39:05 +0530

    initial commit

--- frontend/src/app/request-recommendation/request-recommendation.component.ts
+++ frontend/src/app/request-recommendation/request-recommendation.component.ts
@@ -8,6 +8,14 @@ import { User } from '../user';
   styleUrls: ['./request-recommendation.component.css']
 })
 export class RequestRecommendationComponent implements OnInit {
+
+acc_type=[
+  "1 BHK","2 BHK","3 BHK","PG","Sharing","Single Room"
+]
+
+  amenities=[
+    "Hospital","School","Wifi","Theatres","Parks","Shopping Malls"
+  ]
   user:User=new User();
   constructor(private requestService: RequestserviceService ) {
 
